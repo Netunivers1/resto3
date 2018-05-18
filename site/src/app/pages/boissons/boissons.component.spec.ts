9bb808383cadbed873aaa9e1732bd41285ac0bd0
Wisesha <wise@shasama.com>
2018-05-18 18:12:22 +0300

    add sub menu

---
+++ site/src/app/pages/boissons/boissons.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoissonsComponent } from './boissons.component';
+
+describe('BoissonsComponent', () => {
+  let component: BoissonsComponent;
+  let fixture: ComponentFixture<BoissonsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BoissonsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoissonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
